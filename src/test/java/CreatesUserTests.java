5fe37d809ebd51158430047bac50fa26ba1aaf52
Evgeniya Shiyanova <evgenshiy@gmail.com>
2024-11-14 00:38:01 +0500

    added CreatesUserTests

---
+++ src/test/java/CreatesUserTests.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+public class CreatesUserTests {
+
+    @Test
+    void unsuccessfulRegistration400Test() {
+        String authData = "{\"username\": \"Jane Air\", \"email\": \"Jane@Air.com\", \"password\": \"111\"}";
+
+        given()
+                .contentType("application/json")
+                .body(authData)
+                .log().uri()
+
+                .when()
+                .post("https://reqres.in/api/register")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Note: Only defined users succeed registration"));
+    }
+}
