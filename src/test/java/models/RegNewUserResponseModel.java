1ce96ba857c3eedc2e1e439f0f5764741b1fb33d
Evgeniya Shiyanova <evgenshiy@gmail.com>
2024-11-21 01:07:17 +0500

    updated RegNewUserBodyModel, RegNewUserResponseModel

--- src/test/java/models/RegNewUserResponseModel.java
+++ src/test/java/models/RegNewUserResponseModel.java
@@ -1,31 +1,8 @@
 package models;
 
+import lombok.Data;
+
+@Data
 public class RegNewUserResponseModel {
-
     String id, token, error;
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getError() {
-        return error;
-    }
-
-    public void setError(String error) {
-        this.error = error;
-    }
-
 }
